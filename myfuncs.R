c9196edf9d484357623079845a2ff331d0c96595
kendog <brian.on.iow@gmail.com>
2020-07-01 12:07:09 +0100

    Added myfuncs folder

---
+++ myfuncs.R
@@ -0,0 +1,8 @@
+roll <- function(numdice, rep = TRUE){
+  rolled <- "number of dice must be numeric and > 0"
+  if (is.numeric(numdice) & numdice > 0){
+    die <- 1:6
+    rolled <- sample(die, numdice, replace = rep)
+  }
+  return(rolled)
+}   
