7f4459d63803cce4d12f7a733f539fd740682dc9
kendog <brian.on.iow@gmail.com>
2020-11-25 19:21:43 +0000

    Minor changes

--- Example_of_group_by.R
+++ Example_of_group_by.R
@@ -6,9 +6,14 @@ dset
 dset %>% select(mpg, cyl, hp) %>% 
          group_by(cyl) %>% 
          # ...and then for each group evaluate 
-         summarise(Avg_mpg = mean(mpg), Avg_hp = mean(hp)) 
+         summarise(Avg_mpg = mean(mpg), Avg_hp = mean(hp), Min_hp = min(hp), Max_hp = max(hp)) 
 
 mtcars %>%
   summarise(mean = mean(mpg), n = n())
 
 # ?summarise
+
+dset %>% 
+  group_by(cyl) %>%
+  select(mpg, cyl, hp) %>% 
+  summarise(Avg_mpg = mean(mpg), Avg_hp = mean(hp), Min_hp = min(hp), Max_hp = max(hp))
